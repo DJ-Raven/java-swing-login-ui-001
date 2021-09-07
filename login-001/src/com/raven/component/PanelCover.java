cb1f121ff3f723ca726264791ff162b7be3a8e93
laing raven <laingraven.java@gmail.com>
2021-09-08 01:16:35 +0700

    first commit

---
+++ login-001/src/com/raven/component/PanelCover.java
@@ -0,0 +1,139 @@
+package com.raven.component;
+
+import com.raven.swing.ButtonOutLine;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import javax.swing.JLabel;
+import net.miginfocom.swing.MigLayout;
+
+public class PanelCover extends javax.swing.JPanel {
+
+    private final DecimalFormat df = new DecimalFormat("##0.###");
+    private ActionListener event;
+    private MigLayout layout;
+    private JLabel title;
+    private JLabel description;
+    private JLabel description1;
+    private ButtonOutLine button;
+    private boolean isLogin;
+
+    public PanelCover() {
+        initComponents();
+        setOpaque(false);
+        layout = new MigLayout("wrap, fill", "[center]", "push[]25[]10[]25[]push");
+        setLayout(layout);
+        init();
+
+    }
+
+    private void init() {
+        title = new JLabel("Welcome Back!");
+        title.setFont(new Font("sansserif", 1, 30));
+        title.setForeground(new Color(245, 245, 245));
+        add(title);
+        description = new JLabel("To keep connected with us please");
+        description.setForeground(new Color(245, 245, 245));
+        add(description);
+        description1 = new JLabel("login with your personal info");
+        description1.setForeground(new Color(245, 245, 245));
+        add(description1);
+        button = new ButtonOutLine();
+        button.setBackground(new Color(255, 255, 255));
+        button.setForeground(new Color(255, 255, 255));
+        button.setText("SIGN IN");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                event.actionPerformed(ae);
+            }
+        });
+        add(button, "w 60%, h 40");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 327, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        GradientPaint gra = new GradientPaint(0, 0, new Color(35, 166, 97), 0, getHeight(), new Color(22, 116, 66));
+        g2.setPaint(gra);
+        g2.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(grphcs);
+    }
+
+    public void addEvent(ActionListener event) {
+        this.event = event;
+    }
+
+    public void registerLeft(double v) {
+        v = Double.valueOf(df.format(v));
+        login(false);
+        layout.setComponentConstraints(title, "pad 0 -" + v + "% 0 0");
+        layout.setComponentConstraints(description, "pad 0 -" + v + "% 0 0");
+        layout.setComponentConstraints(description1, "pad 0 -" + v + "% 0 0");
+    }
+
+    public void registerRight(double v) {
+        v = Double.valueOf(df.format(v));
+        login(false);
+        layout.setComponentConstraints(title, "pad 0 -" + v + "% 0 0");
+        layout.setComponentConstraints(description, "pad 0 -" + v + "% 0 0");
+        layout.setComponentConstraints(description1, "pad 0 -" + v + "% 0 0");
+    }
+
+    public void loginLeft(double v) {
+        v = Double.valueOf(df.format(v));
+        login(true);
+        layout.setComponentConstraints(title, "pad 0 " + v + "% 0 " + v + "%");
+        layout.setComponentConstraints(description, "pad 0 " + v + "% 0 " + v + "%");
+        layout.setComponentConstraints(description1, "pad 0 " + v + "% 0 " + v + "%");
+    }
+
+    public void loginRight(double v) {
+        v = Double.valueOf(df.format(v));
+        login(true);
+        layout.setComponentConstraints(title, "pad 0 " + v + "% 0 " + v + "%");
+        layout.setComponentConstraints(description, "pad 0 " + v + "% 0 " + v + "%");
+        layout.setComponentConstraints(description1, "pad 0 " + v + "% 0 " + v + "%");
+    }
+
+    private void login(boolean login) {
+        if (this.isLogin != login) {
+            if (login) {
+                title.setText("hello, Friend!");
+                description.setText("Enter your personal details");
+                description1.setText("and start journey with us");
+                button.setText("SIGN UP");
+            } else {
+                title.setText("Welcome Back!");
+                description.setText("To keep connected with us please");
+                description1.setText("login with your personal info");
+                button.setText("SIGN IN");
+            }
+            this.isLogin = login;
+        }
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
---
+++ login-001/src/com/raven/main/Main.java
@@ -0,0 +1,166 @@
+package com.raven.main;
+
+import com.raven.component.PanelCover;
+import com.raven.component.PanelLoginAndRegister;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import net.miginfocom.swing.MigLayout;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
+
+public class Main extends javax.swing.JFrame {
+
+    private MigLayout layout;
+    private PanelCover cover;
+    private PanelLoginAndRegister loginAndRegister;
+    private boolean isLogin;
+    private final double addSize = 30;
+    private final double coverSize = 40;
+    private final double loginSize = 60;
+    private final DecimalFormat df = new DecimalFormat("##0.###");
+
+    public Main() {
+        initComponents();
+        init();
+    }
+
+    private void init() {
+        layout = new MigLayout("fill, insets 0");
+        cover = new PanelCover();
+        loginAndRegister = new PanelLoginAndRegister();
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                double fractionCover;
+                double fractionLogin;
+                double size = coverSize;
+                if (fraction <= 0.5f) {
+                    size += fraction * addSize;
+                } else {
+                    size += addSize - fraction * addSize;
+                }
+                if (isLogin) {
+                    fractionCover = 1f - fraction;
+                    fractionLogin = fraction;
+                    if (fraction >= 0.5f) {
+                        cover.registerRight(fractionCover * 100);
+                    } else {
+                        cover.loginRight(fractionLogin * 100);
+                    }
+                } else {
+                    fractionCover = fraction;
+                    fractionLogin = 1f - fraction;
+                    if (fraction <= 0.5f) {
+                        cover.registerLeft(fraction * 100);
+                    } else {
+                        cover.loginLeft((1f - fraction) * 100);
+                    }
+                }
+                if (fraction >= 0.5f) {
+                    loginAndRegister.showRegister(isLogin);
+                }
+                fractionCover = Double.valueOf(df.format(fractionCover));
+                fractionLogin = Double.valueOf(df.format(fractionLogin));
+                layout.setComponentConstraints(cover, "width " + size + "%, pos " + fractionCover + "al 0 n 100%");
+                layout.setComponentConstraints(loginAndRegister, "width " + loginSize + "%, pos " + fractionLogin + "al 0 n 100%");
+                bg.revalidate();
+            }
+
+            @Override
+            public void end() {
+                isLogin = !isLogin;
+            }
+        };
+        Animator animator = new Animator(800, target);
+        animator.setAcceleration(0.5f);
+        animator.setDeceleration(0.5f);
+        animator.setResolution(0);  //  for smooth animation
+        bg.setLayout(layout);
+        bg.add(cover, "width " + coverSize + "%, pos 0al 0 n 100%");
+        bg.add(loginAndRegister, "width " + loginSize + "%, pos 1al 0 n 100%"); //  1al as 100%
+        cover.addEvent(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!animator.isRunning()) {
+                    animator.start();
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bg = new javax.swing.JLayeredPane();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        bg.setBackground(new java.awt.Color(255, 255, 255));
+        bg.setOpaque(true);
+
+        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
+        bg.setLayout(bgLayout);
+        bgLayout.setHorizontalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 933, Short.MAX_VALUE)
+        );
+        bgLayout.setVerticalGroup(
+            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 537, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg, javax.swing.GroupLayout.Alignment.TRAILING)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(bg)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLayeredPane bg;
+    // End of variables declaration//GEN-END:variables
+}
